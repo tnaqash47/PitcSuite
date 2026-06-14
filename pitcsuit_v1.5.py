ee9c8744129b83e376b53a32f109bfa276ea027c
tahirnaqash004 <tahir.naqash004@gmail.com>
2026-06-14 21:00:43 +0500

    reverted

--- pitcsuit_v1.5.py
+++ pitcsuit_v1.5.py
@@ -30,8 +30,6 @@ QMainWindow, QWidget {
     color: #e0e6f0;
     font-family: 'Consolas', 'Courier New', monospace;
     font-size: 12px;
-    fgadfasdf
-    adfasf
 }
 QGroupBox {
     border: 1px solid #2e3340;
